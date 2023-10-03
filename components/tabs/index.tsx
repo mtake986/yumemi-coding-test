19b4861076b281297cecb64f986f66ba945edb56
mtake986 <tosa9629@gmail.com>
2023-10-02 20:53:38 -0600

    Context APIとテストを組み合わせることができた。TabsとWhenPplDataFetchingのテストを書いた。もちろんエラーなし。そして、カスタムでのレンダー関数を書き、それをインポートして使ってる

--- components/tabs/index.tsx
+++ components/tabs/index.tsx
@@ -4,7 +4,7 @@ import Tab from "./tab";
 import styles from "./tabs.module.css";
 const Tabs = () => {
   return (
-    <section className={styles.container}>
+    <section data-testid='section' className={styles.container}>
       {tabValues.map((tabValue) => (
         <Tab key={tabValue.id} tabValue={tabValue} />
       ))}
